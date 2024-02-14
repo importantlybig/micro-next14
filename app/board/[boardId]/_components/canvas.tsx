e98fc912794a6f4e1b5df0a9b679dc2138fe4846
Dai Phan <dai.phan@southasiaservices.com>
2024-02-14 22:54:23 +0700

    add selection tools

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -35,6 +35,7 @@ import { nanoid } from 'nanoid';
 import { LiveObject } from '@liveblocks/client';
 import { LayerPreview } from './layer-preview';
 import { SelectionBox } from './selection-box';
+import { SelectionTools } from './selection-tools';
 
 const MAX_LAYERS = 100;
 
@@ -404,6 +405,7 @@ const Canvas = ({ boardId }: CanvasProps) => {
 				undo={history.undo}
 				redo={history.redo}
 			/>
+			<SelectionTools camera={camera} setLastUsedColor={setLastUsedColor} />
 			<svg
 				className='h-[100vh] w-[100vw]'
 				onWheel={onWheel}
